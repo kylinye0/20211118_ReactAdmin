fd76d5384e09d8085ac46094371a2fc58e7528cb
YK <724456525@qq.com>
2021-11-18 08:18:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
+import './App.less'
+export default class App extends Component{
+    render() {
+        return (
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Admin />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                </Routes>
+            </BrowserRouter>
+
+        );
+    }
+}
