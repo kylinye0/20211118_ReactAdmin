374aea67c627a9fa6c2803d7523258d17d566ab4
YK <724456525@qq.com>
2021-12-07 16:55:55 +0800

    dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 /**能更具接口文档定义接口请求*/
+import jsonp from 'jsonp';
 import ajax from './ajax';
 /*export function reqLogin() {
     return ajax('/login',{username,password},'POST');
@@ -13,3 +14,21 @@ export const reqAddUser = (user)=>ajax(BASE+'/manage/user/add',user,'POST');
 export const reqItoperation =(id)=>ajax(BASE+'/ItOperations',{"id":id},'GET');
 export const reqAddItope = (itoperation)=>ajax(BASE+'/ItOperations',itoperation,'POST');
 export const reqRemoveItope = (id)=>ajax(BASE+'/ItOperations/'+id,{"id":id},'DELETE');
+
+export  const reqWeather=(id)=>{
+    const url = 'https://api.map.baidu.com/weather/v1/?district_id='+id+'&data_type=all&ak=2qXSvTfUwm2LWeQ4SRQ8E0XRZysvqWvA';
+    jsonp(url,{},(err,data)=>{
+        console.log('json()',data);
+        if(!err&&data.status===0)
+        {
+         // const {text,uptime}=  data.result[0].now[0];
+        }
+        else
+        {
+            console.log('shibai'+err);
+        }
+    })
+};
+
+reqWeather(331023);
+/*json请求的接口请求函数*/
--- src/components/mainheader-nav/index.jsx
+++ src/components/mainheader-nav/index.jsx
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import './index.less';
-import {Breadcrumb} from "antd";
+//import {Breadcrumb} from "antd";
 //@author yekai 724456525@qq.com
 export default class Mainheadernav extends Component{
     render() {
@@ -19,7 +19,10 @@ export default class Mainheadernav extends Component{
                     <div className="header-bottom-left">首页</div>
                     <div className="header-bottom-right">
                         <span>2021-12-06</span>
-                        <img src="weather.gif" alt="weather" />
+                        <iframe width="430" scrolling="no" height="25" frameBorder="0" allowTransparency="true"
+                                src="https://i.tianqi.com?c=code&id=1&icon=1&site=12" title="weather"></iframe>
+                       {/* <iframe width="400" height="100" frameBorder="0" scrolling="no" hspace="0"
+                                src="https://i.tianqi.com/?c=code&a=getcode&id=35&site=34&icon=1" title="tianqi"></iframe>*/}
                         <span>晴</span>
                     </div>
                 </div>
