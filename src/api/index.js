94ba21e3268bc1a26d89a898d20e384baa49a7b3
YK <724456525@qq.com>
2021-12-08 17:02:13 +0800

    dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 /**能更具接口文档定义接口请求*/
 import jsonp from 'jsonp';
+import crypto from 'crypto';
 import ajax from './ajax';
+import urlencode from 'urlencode';
 /*export function reqLogin() {
     return ajax('/login',{username,password},'POST');
 }
@@ -16,10 +18,49 @@ export const reqAddItope = (itoperation)=>ajax(BASE+'/ItOperations',itoperation,
 export const reqRemoveItope = (id)=>ajax(BASE+'/ItOperations/'+id,{"id":id},'DELETE');
 
 export  const reqWeather=(location)=>{
-   // const url = 'https://api.map.baidu.com/weather/v1/?district_id='+id+'&data_type=all&ak=2qXSvTfUwm2LWeQ4SRQ8E0XRZysvqWvA';
-    const url = 'https://api.seniverse.com/v3/weather/now.json?key=S4jAeOgEk4Y3PuWWE&location='+location+'&language=zh-Hans&unit=c';
+    var querystring = require('querystring');
+    var ENDPOINT='http://api.seniverse.com/v3/weather/now.json';
+   // var str="ts=1443079775&ttl=300&uid=PLNmAelOgsBIiIH-u";
+    var str ="location=%E5%8F%B0%E5%B7%9E&language=zh-Hans&unit=c&ts=1443079775&ttl=300&uid=PLNmAelOgsBIiIH-u";
+    var sha = crypto.createHmac("sha1",'S4jAeOgEk4Y3PuWWE');
+    var base = sha.update(str).digest('base64');
+    //base ="Mjk4OTY0NGI5ZDg5ZjIzOWE2YmUzODY1MzkxMDgyZWI0ZWE1MWZmZQ==";
+   var urlencode1 = urlencode(base) ;
+   // console.log('hmac '+sha.digest());
+    console.log('base '+base);
+    console.log('urlenc '+urlencode1);
+    var obj ={
+
+        location:location,
+        language:'zh-Hans',
+        unit:'c',
+        ts:'1443079775',
+        ttl:'300',
+        uid:'PLNmAelOgsBIiIH-u',
+        sig:urlencode1,
+    };
+    var q = querystring.encode(obj);
+ //  const url = 'https://api.map.baidu.com/weather/v1/?district_id='+location+'&data_type=all&ak=2qXSvTfUwm2LWeQ4SRQ8E0XRZysvqWvA';
+   // const url = 'http://api.seniverse.com/v3/weather/now.json?key=S4jAeOgEk4Y3PuWWE&location='+location+'&language=zh-Hans&unit=c&sig='+urlencode1;
+    const url = 'https://api.seniverse.com/v3/weather/now.json?location='+location+'&language=zh-Hans&unit=c&uid=PLNmAelOgsBIiIH-u&sig='+urlencode1;
+    // 提供jsonp服务的url地址;
+    //var url = "http://api.map.baidu.com/place/v2/search?query=ATM机&tag=银行&region=北京&output=json&ak=F552bedbee2ec8fa6bae7b7a08201&callback=callback";
+
+    // // 创建script标签，设置其属性;
+    // var script = document.createElement("script");
+    // script.setAttribute("src", url);
+    // document.getElementsByTagName("head")[0].appendChild(script);
+    //
+    // // 得到查询结果后的回调函数;
+    // var callback = function (data) {
+    //     var json = JSON.stringify(data.results[0]);
+    //     console.log(json);
+    // };
+    //
+    //ENDPOINT+'?'+q
+
     jsonp(url,{},(err,data)=>{
-        console.log('json()',JSON.stringify(data));
+        console.log('json()',url);
         if(!err&&data.status===0)
         {
          const {text,code}=  data.results[0].now[0];
